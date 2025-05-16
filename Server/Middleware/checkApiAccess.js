85fa0eef72a4667bdac0d582c0281c31f357c9bc
anuragco <anurag.console@gmail.com>
2025-05-16 17:57:45 +0530

    fix: Correct header key for registration number and update log API usage paths

--- Server/Middleware/checkApiAccess.js
+++ Server/Middleware/checkApiAccess.js
@@ -3,10 +3,10 @@ const {logApiUsage} = require('../Utils/apislogger');
 
 
 async function checkApiAccess(req, res, next) {
-  const registrationNumber = req.headers['registration_number'];
+  const registrationNumber = req.headers['registration-number'];
 
   if (!registrationNumber) {
-    await logApiUsage(req, '/ask-gemini', 'POST', req.body, { error: "Missing registration number" }, 400);
+    await logApiUsage(req, '/api/v3/modal/ai', 'POST', req.body, { error: "Missing registration number" }, 400);
     return res.status(400).json({ error: 'Missing registration number' });
   }
 
@@ -17,14 +17,14 @@ async function checkApiAccess(req, res, next) {
     );
 
     if (rows.length === 0) {
-      await logApiUsage(req, '/ask-gemini', 'POST', req.body, { error: "Not Authorized" }, 401);
+      await logApiUsage(req, '/api/v3/modal/ai', 'POST', req.body, { error: "Not Authorized" }, 401);
       return res.status(404).json({ error: 'Not Authorized' });
     }
 
     const user = rows[0];
 
     if (user.api_used >= user.api_limit) {
-      await logApiUsage(req, '/ask-gemini', 'POST', req.body, { error: "Plan expired." }, 403);
+      await logApiUsage(req, '/api/v3/modal/ai', 'POST', req.body, { error: "Plan expired." }, 403);
       return res.status(403).json({ error: 'API limit reached. Plan expired.' });
     }
 
@@ -34,7 +34,7 @@ async function checkApiAccess(req, res, next) {
     next();
   } catch (err) {
     console.error('Error in API access middleware:', err);
-    await logApiUsage(req, '/ask-gemini', 'POST', req.body, { error: "Internal server error" }, 500);
+    await logApiUsage(req, '/api/v3/modal/ai', 'POST', req.body, { error: "Internal server error" }, 500);
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
